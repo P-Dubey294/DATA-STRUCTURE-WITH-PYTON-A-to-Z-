bd753a7cd34bb4bb073559b6e9c75a7c5ebba1e5
P-Dubey294 <dpratham649@gmail.com>
2025-06-23 12:57:58 +0530

    Add initial implementation of comparison operators examples in Python

---
+++ OPERATORS/Comparision Operator.py
@@ -0,0 +1,13 @@
+num1 = 10;
+num2 = 20;
+print(num1 == num2)
+print(num1 != num2)
+print(num1 > num2)
+print(num1 < num2)
+print(num1 >= num2)
+print(num1 <= num2)
+
+pos_inf = float('inf')
+neg_inf = float('-inf')
+print(pos_inf == neg_inf)
+print (pos_inf > neg_inf)
