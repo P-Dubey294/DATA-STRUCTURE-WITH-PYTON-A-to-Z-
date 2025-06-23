fd1d17fab3cebc802e3b9a86fd7462e382f4ef62
P-Dubey294 <dpratham649@gmail.com>
2025-06-23 12:37:54 +0530

    Add initial implementation of arithmetic operators examples in Python

---
+++ OPERATORS/Airthmatic Operators.py
@@ -0,0 +1,14 @@
+num1 = 10 ;
+num2 = 20 ;
+print (num1 + num2)
+print (num1 - num2)
+print (num1 * num2)
+print (num1 / num2)
+print (23 / 2) #-->  Qotient values
+print (23 % 3) #--> return Remainder value
+
+''' Q)  suppose we have input as number then if we can chechk wethere its is odd or even . If even then print 
+ false otherwise true  '''
+
+num = int(input(" Please Enter the number"))
+print(num%2 ==0)
