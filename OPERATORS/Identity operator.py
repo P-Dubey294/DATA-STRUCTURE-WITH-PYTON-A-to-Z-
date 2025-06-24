74878cf902855a8eb88a8ab65519f3a683dc04b7
P-Dubey294 <dpratham649@gmail.com>
2025-06-24 14:16:59 +0530

    Add examples for membership operators in Python

--- OPERATORS/Identity operator.py
+++ OPERATORS/Identity operator.py
@@ -10,3 +10,8 @@ arr2 = [1,2,3]
 print (arr1 is arr2)
 print (arr1 is not arr2)
 
+#memebership operator 
+my_list = [1,2,3,4,5,6]
+print (5 in my_list)
+print (7 in my_list)
+print ( 5 not in my_list)
