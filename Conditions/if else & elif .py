04107b71abca11f1d3fa2a83c4dbd2b5f0917aeb
P-Dubey294 <dpratham649@gmail.com>
2025-06-24 16:20:38 +0530

    Add initial implementation of conditional statements examples in Python

---
+++ Conditions/if else & elif .py
@@ -0,0 +1,22 @@
+Marks = 25
+if(Marks>=25):
+    print("A")
+elif(Marks>=30):
+    print("B")
+elif(Marks<=25):
+    print("C")
+elif(Marks<=10):
+    print("D")
+else:
+    print("Study More")
+
+
+''' OR '''
+
+age = int (input ("Enter the nmuber"))
+if(age>=18):
+    print("you can vote")
+elif(age<=18):
+    print("you cant vote")
+else:
+    print("Jay Hind Jay Bharat!")
