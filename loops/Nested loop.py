d20ad1b5f60ff04b9202c0fad9792b109fa87228
P-Dubey294 <dpratham649@gmail.com>
2025-06-25 14:14:55 +0530

    Add initial implementation of nested loops examples in Python

---
+++ loops/Nested loop.py
@@ -0,0 +1,11 @@
+for i in range (10): #outer loop
+    for j in range (5): # innner loop
+        print ("hello pratham dubey")
+
+        ''' OR '''
+
+for a in range (4):
+    for b in range (1,13):
+        if(b%4==0):
+            break #continue
+        print(b)
