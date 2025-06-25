5352ab6e6680ecd1e1c140584b37b960083aeb00
P-Dubey294 <dpratham649@gmail.com>
2025-06-25 14:14:49 +0530

    Add initial implementation of for loop examples in Python

---
+++ loops/for loop.py
@@ -0,0 +1,29 @@
+fruits = [ "Apple" , "Banana" , "Orange" , "Pinapple" , "Mangoes"]
+for fruits in fruits :
+    print (fruits)
+
+name = "pratham"
+for ch in name :
+    print(ch, end=" ")
+
+''' Range function and its variations'''
+#1>
+ 
+for i in range (5) :
+    print (i)
+
+#2nd>
+
+for i in range (2 ,4):
+    print (i)
+
+#3rd>
+
+for i in range ( 2,10,2):
+    print(i)
+
+for j in range ( 10 ,0 ,-1):
+    print (j)
+
+for k in range (12,0,-3):
+    print (k)
