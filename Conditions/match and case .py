ec9c92073c54679064ff28a83059d43f0ac63c00
P-Dubey294 <dpratham649@gmail.com>
2025-06-25 10:49:27 +0530

    Add initial implementation of match-case statement examples in Python

---
+++ Conditions/match and case .py
@@ -0,0 +1,16 @@
+week_num = 2 
+match week_num :
+    case 1:
+        print(" Today is sunday")
+    case 2: 
+        print("today is monday")
+    case 3:
+        print("today is tuesaday")
+    case 4: 
+        print("today is wednesday")
+    case 5:
+        print("today is thursaday")
+    case 6:
+        print(" today is friday")
+    case 7:
+        print (" today is saturday")
