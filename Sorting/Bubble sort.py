cee49c7b55881329d62e89a1feff3d57d04e364b
P-Dubey294 <dpratham649@gmail.com>
2025-07-08 14:34:31 +0530

    Fix logic error in enhanced Bubble Sort implementation

--- Sorting/Bubble sort.py
+++ Sorting/Bubble sort.py
@@ -25,7 +25,8 @@ def Bubble_sort_enhanced(arr):
     for i in range(n-1):
         is_sorted = True
         for j in range(n-i-1):
-            if arr[j] > arr[j+1]:
+             is_sorted = True
+             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
                 is_sorted = False
         if is_sorted:
