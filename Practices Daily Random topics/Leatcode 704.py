f90a918805a8a8cd63e70de30700b8a182dc7a37
P-Dubey294 <dpratham649@gmail.com>
2025-07-14 13:18:54 +0530

    Add implementation of Binary Search algorithm

---
+++ Practices Daily Random topics/Leatcode 704.py
@@ -0,0 +1,19 @@
+def BinarySerch(nums, target):
+    low = 0 
+    high = len(nums)-1
+    while(low <= high):
+        mid = (low + high) // 2 
+        if(nums[mid] == target):
+            return mid 
+        elif nums[mid] < target:
+            low = mid + 1 
+        else:
+            high  = mid - 1
+    return -1 
+
+nums = [-1 , 0 , 3 ,5 ,9 ,12]
+print(nums)
+target = 9
+print(BinarySerch(nums, target))
+
+
