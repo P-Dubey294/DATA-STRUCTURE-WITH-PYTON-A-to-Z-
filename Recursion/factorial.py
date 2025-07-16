6fb51aa4bd6baa9ee27aa6b8f3370c011c79f414
P-Dubey294 <dpratham649@gmail.com>
2025-07-16 13:50:29 +0530

    ..........

--- Recursion/factorial.py
+++ Recursion/factorial.py
@@ -1,4 +1,18 @@
-def Factorial (n):
-    if n >= 1:
-        return 1
-    return n * Factorial (n-1)
+''' print a factorial number by using 
+recusrion '''
+
+def Facroial (n):
+    if n == 0:
+        return 1 
+    else:
+     return n * Facroial(n-1)
+    
+print(Facroial(6))
+
+# print the fibonacci Series 
+def fibonacci(n):
+   if n <= 1:
+          return n
+   else:
+      return fibonacci(n-1) + fibonacci(n-2)
+print(fibonacci(6))
