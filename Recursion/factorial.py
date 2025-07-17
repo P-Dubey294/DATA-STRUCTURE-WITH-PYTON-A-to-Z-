fdb27bda0cd80b00d6961e869547960954fa9564
P-Dubey294 <dpratham649@gmail.com>
2025-07-17 13:30:04 +0530

    recursion problems

--- Recursion/factorial.py
+++ Recursion/factorial.py
@@ -16,3 +16,25 @@ def fibonacci(n):
    else:
       return fibonacci(n-1) + fibonacci(n-2)
 print(fibonacci(6))
+
+# print the sum of array 
+def SumofArray(arr , n):
+    if n == 0:
+        return 0
+    else:
+        return SumofArray(arr, n-1) + arr[n-1]
+    
+arr =[1,2,3,4,5]
+print(SumofArray(arr,len(arr)))
+
+# Number of ways to climb Strair 
+def Stair(n):
+    if n == 0 :
+        return 1
+    if n < 0 : 
+        return 0
+    else:
+        return Stair(n-1)+ Stair(n-2)
+    
+n = 5
+print(Stair(n))
