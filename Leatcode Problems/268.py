5c0ab72a306e743534c3c202cda75aed538fb8f2
P-Dubey294 <dpratham649@gmail.com>
2025-07-25 10:43:17 +0530

    leatcode(268) finding missing element

--- Leatcode Problems/268.py
+++ Leatcode Problems/268.py
@@ -9,3 +9,25 @@ def find_missing(nums):
 
 data =  [0,2,3,4,1,6,7,8,9]
 print(find_missing(data))
+
+'''Time complexity is 0(nlogn)'''
+
+#using cycyle sort 
+def Finding_MIssing(nums):
+    n = len(nums)
+    i = 0 
+    
+    while i < n :
+        correct_index = nums[i]
+        if (nums[i] < n )and nums[i] != nums[correct_index]:
+            nums[i], nums[correct_index] , = nums[correct_index], nums[i]
+        else:
+            i+=1
+
+        for i in range(n):
+            if nums[i] != i:
+                return i  
+            
+        return n
+arr = [0,1,2,5,6,7,3]
+print(Finding_MIssing(arr))
