6fcfab742194785fbe8e13f85945dbdf41ff9825
P-Dubey294 <dpratham649@gmail.com>
2025-08-19 10:01:12 +0530

    implementation of stack using linkedlist

--- Stack Python/implementation.py
+++ Stack Python/implementation.py
@@ -1,25 +1,24 @@
 class Stack:
-    # this is a constrcutor 
     def __init__(self):
         self.stack = []
-    # this is a push operation
-    def Push(self, value):
+
+    def push(self,value):
         self.stack.append(value)
-    # this is pop opperation 
+    
     def pop(self):
-        self.stack.pop()
-    # print result function
-    def printResult(self):
+        self.stack.pop(0)
+    
+    def result(self):
         return self.stack
     
-s = Stack()
-s.Push(10)
-s.Push(20)
-s.Push(30)
-s.Push(40)
-s.Push(50)
+S = Stack()
 
-print("Pusing the Element :- ",s.printResult())
-s.pop()
-print("Poping the Element :- ",s.printResult())
+S.push(10)
+S.push(20)
+S.push(30)
+S.push(40)
+S.push(50)
 
+print("After Pusing",S.result())
+S.pop()
+print("After poping", S.result())
