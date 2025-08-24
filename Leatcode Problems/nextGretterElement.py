a71db3020f5e1f2ea25185802f48a95dec0fa156
P-Dubey294 <dpratham649@gmail.com>
2025-08-24 15:25:50 +0530

    ............

---
+++ Leatcode Problems/nextGretterElement.py
@@ -0,0 +1,12 @@
+nums = [1,0,13,17,20,21,8]
+stack = []
+result = [-1] * len(nums)
+
+for i in range(len(nums)-1, -1, -1):
+    while stack and nums[i] >= stack[-1]:
+        stack.pop()
+    if len(stack)!=0:
+        result[i] = stack[-1]
+    stack.append(nums[i])
+
+print(result)
