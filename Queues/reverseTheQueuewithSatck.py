f9faee087bed1e9804710e05fc4b33659195bc24
P-Dubey294 <dpratham649@gmail.com>
2025-08-26 13:30:16 +0530

    reversed the queue with the help of stack []

--- Queues/reverseTheQueuewithSatck.py
+++ Queues/reverseTheQueuewithSatck.py
@@ -1,15 +1,14 @@
 from collections import deque
-
-def reverse_dequeue(q):
+def reverse_queue(q):
     stack = []
-#push the element queue to stack
+
     while q:
         stack.append(q.popleft())
     
-#pop the element stack to queue
     while stack:
         q.append(stack.pop())
+    
     return q
 
-queue = reverse_dequeue(deque([1,2,3,4,5,6,7,8,9]))
+queue = reverse_queue(deque([1,2,3,4,5,6,7,8,9]))
 print(queue)
